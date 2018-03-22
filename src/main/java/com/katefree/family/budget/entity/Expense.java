97dd02f8260fc52666ef95a0235d17f7b1de5a79
Ekaterina Tsapaeva <etsapaeva@gmail.com>
2018-03-22 14:59:01 +0300

    Spring Data added

---
+++ src/main/java/com/katefree/family/budget/entity/Expense.java
@@ -0,0 +1,31 @@
+package com.katefree.family.budget.entity;
+
+import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+/**
+ * Expense
+ *
+ * @author Ekaterina Tsapaeva
+ * @link http://jira.taskdata.com/browse/
+ * @since 22/03/2018
+ */
+@Data
+@Entity
+public class Expense {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private BigDecimal amount;
+    private String description;
+
+    private Expense() {
+    }
+
+}
